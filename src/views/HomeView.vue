25b2384fb3bd772a6631d1ebe8b94d2e17be8400
Filipe Oliveira <t-filipe.oliveira@matrixenergia.com>
2023-07-05 17:12:20 -0300

    feat: add NewsList to Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,10 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <img alt="Live News" class="w-32 ml-auto mr-auto my-4" src="../assets/svg/live-news.svg">
+    <NewsList/>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
+<script setup lang="ts">
+  import NewsList from '@/components/organisms/NewsList.vue'
 </script>
