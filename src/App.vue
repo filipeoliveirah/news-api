15e3289901b439b670de15e28adf8aa8755d7cd1
Filipe Oliveira <t-filipe.oliveira@matrixenergia.com>
2023-07-05 11:22:56 -0300

    chore: remove default style in app.vue

--- src/App.vue
+++ src/App.vue
@@ -6,25 +6,9 @@
   <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script setup lang="ts">
+  import { useStore } from 'vuex';
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+  const store = useStore()
+  store.dispatch('fetchNews')
+</script>
